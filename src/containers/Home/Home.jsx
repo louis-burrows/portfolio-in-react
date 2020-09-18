ae4a4a1494516ba90b96046aa45313ff7e4c529b
Your Name <you@example.com>
2020-09-18 17:45:27 +0100

    the circle still grows, but major changes will be made

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -4,21 +4,27 @@ import styles from "./Home.module.scss"
 class Home extends Component {
   state = {
     circleIsExpanded: false,
+    showTreeInCircle: false,
   };
 
 
   expandCircle = () => {
     this.setState({ circleIsExpanded: !this.state.circleIsExpanded });
+    this.setState({ showTreeInCircle: !this.state.showTreeInCircle });
   };
+
+
 
   render() { 
 
     const flipStyles = this.state.circleIsExpanded ? styles.openCircle : "";
 
+    const showTree = this.state.showTreeInCircle ? styles.treeVisible : "";
+
     return ( 
       <div>
         <h1 className={styles.homeTitle}>Welcome</h1>
-        <span className={`${styles.circle} ${flipStyles}`}
+        <span className={`${styles.circle} ${flipStyles} ${showTree}`}
         onClick={() => this.expandCircle()}></span>
       
       </div>
