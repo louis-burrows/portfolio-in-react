bd7b0f59667f9e9f21dc57f0de046e535bae3311
Your Name <you@example.com>
2020-09-25 16:21:10 +0100

    more styling changes

--- src/containers/Bio/Bio.jsx
+++ src/containers/Bio/Bio.jsx
@@ -1,45 +1,25 @@
 import React, { Component } from 'react';
 import styles from "./Bio.module.scss"
 
+
 class Bio extends Component {
   state = {  }
+
+  
   render() { 
+
+    
+
     return ( 
-      <div>
-        {/* <Link to="../../../public/files/cv.pdf" target="_blank" download>Download</Link> */}
-        <h1 className={styles.bioTitle}>Biography</h1>
-        <span>
 
-          <p className={styles.slow}>
-            An ambitious, driven and self-motivated developer, who is experienced in administration, verbal and written communication techniques, and who values excellent customer and client service and wants to make his career a lifelong process of learning and self-improvement.
-          </p>
-
-          {/* <button>Read More</button>
-
-          <p className={styles.intro}> 
-            My interest in software development began at university when I realised the potential of being able to develop tailored applications for the collection and interpretation of vast data sets. My introduction to the industry truly commenced amidst an intensive coding boot camp, where I was introduced to an array of languages and libraries. Since that experience, my interest and desire to learn and contribute have increased exponentially, as every new skill has widened my perception of what is possible.
-          </p>
-
-          <p className={styles.intro}>
-            I have worked as a teacher, vehicle engineer, bakery manager, research geneticist and in administration and management. 
-          </p>
-
-          <p className={styles.intro}>
-            My experience to date has exemplified the importance of developing and maintaining good working relationships with both colleagues and service users, and I consider good communication to be integral to this and productive teamwork. I believe that good business and customer relations are paramount to providing a distinguished service. I have a positive attitude and I am always willing to learn and develop
-          </p> */}
+      <div className={styles.bioContainer}>
+      
+        <h1 className={styles.bioTitle}>
+          Biography        
+        </h1>
+        <p className={styles.slow}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quae dolores quaerat quidem ad, pariatur earum modi alias! Facilis hic optio saepe excepturi quam laudantium repellendus aperiam inventore laboriosam neque!</p>
         
-        {/* </span>
-
-        <span>
-
-          <h2>Personal Interests</h2>
-
-          <p>
-
-          </p>*/}
-
-        </span> 
-
+       
       </div>
      );
   }
--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -6,13 +6,14 @@ class Contact extends Component {
   render() { 
     return ( 
       <div className={styles.contactPage}>
-        <h2>Contact Page</h2>
-        <p className={styles.title}>Email: </p>
-        <p>louisburrows@gmail.com</p>
-        <p className={styles.title}>Linked-in: </p>
-        <p>https://www.linkedin.com/in/louis-burrows-100727170/</p>
-        <p className={styles.title}>Facebook: </p>
-        <p>https://www.facebook.com/louis.burrows.3363</p>     
+
+        <h2 className={styles.contactTitle}>Contact Information</h2>
+
+        <a href="mailto:louisburrows@gmail.com" className={styles.contactParagraph}>Email me!</a>
+
+        <a href="https://www.linkedin.com/in/louis-burrows-100727170/" target="_blank" className={styles.contactParagraph}>Linked-in</a>
+         
+        <a href="https://github.com/louis-burrows" target="_blank" className={styles.contactParagraph}>Github</a>
       </div>
      );
   }
