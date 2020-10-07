726738db9f2f35f021c67ff7523a4a58fcce0028
Your Name <you@example.com>
2020-10-07 12:53:29 +0100

    flowers added and working

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import styles from "./Home.module.scss"
 import image1 from "../../images/1.png"
 import image2 from "../../images/2.png"
-import image3 from "../../images/3.jpg"
-import image4 from "../../images/4.jpg"
-import { Slide, Fade, Pulse, Flash, AttentionSeeker } from "react-awesome-reveal";
+import image3 from "../../images/3.png"
+
+import { Slide, Fade } from "react-awesome-reveal";
 
 
 class Home extends Component {
@@ -19,25 +19,22 @@ class Home extends Component {
       <div className={styles.homeContainer}>
 
         <Fade delay={2500} duration={2000}>  
-        <h1 className={styles.homeTitle}>Welcome</h1>
+          <h1 className={styles.homeTitle}>Welcome</h1>
         </Fade>
 
-         {/* <Slide triggerOnce delay={1200} duration={2000}> */}
+        <Slide triggerOnce delay={200} duration={1500} className={styles.slide1}>
           <img src={image1} alt="1" className={styles.image1}/>
-        {/* </Slide> */}
- {/*
-        <Slide triggerOnce delay={900} duration={2000}>
+        </Slide>
+ 
+        <Slide triggerOnce delay={900} duration={1500} className={styles.slide2}>
           <img src={image2} alt="2" className={styles.image2}/>
-        </Slide> */}
-        {/*
-        <Slide triggerOnce delay={600} duration={2000}>
+        </Slide> 
+        
+        <Slide triggerOnce delay={1800} duration={1500} className={styles.slide3}>
           <img src={image3} alt="3" className={styles.image3}/>
         </Slide>
         
-        <Slide triggerOnce delay={300} duration={2000}>
-          <img src={image4} alt="4" className={styles.image4}/>
-        </Slide> */}
-     
+       
       
       </div>
      );
