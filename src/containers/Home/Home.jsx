8baedc016af51763934bf77b714660ff11ae559e
Your Name <you@example.com>
2020-10-08 16:22:53 +0100

    making the button do things

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -10,25 +10,37 @@ import { Slide, Fade } from "react-awesome-reveal";
 class Home extends Component {
   state = {
       circleIsExpanded: false,
+      image1Shows: false,
   };
   
-  
+  expandCircle = () => {
+    this.setState({ circleIsExpanded: !this.state.circleIsExpanded });
+    this.setState({ image1Shows: !this.state.image1Shows });
+  };
 
 
   render() { 
+
+    const flipStyles = this.state.circleIsExpanded ? styles.buttonVanish : "";
+
+    const showImage1 = this.state.image1Shows ? styles.slide1 : "";
+
 
     return ( 
       <div className={styles.homeContainer}>
 
         <Fade delay={1000} duration={2000} className={styles.fades}>  
           <h1 className={styles.homeTitle}>Welcome</h1>
-          <button className={styles.growButton}>Make things grow</button>
+          <button className={`${styles.growButton} ${flipStyles}`} onClick={() => this.expandCircle()}>
+            Make things grow
+          </button>
         </Fade>
 
-        {/* <Slide triggerOnce delay={2000} duration={1500} className={styles.slide1}>
+         <Slide triggerOnce delay={200} duration={1500} className={showImage1}>
           <img src={image1} alt="1" className={styles.image1}/>
         </Slide>
- 
+        
+        {/*
         <Slide triggerOnce delay={2600} duration={1500} className={styles.slide2}>
           <img src={image2} alt="2" className={styles.image2}/>
         </Slide>   
