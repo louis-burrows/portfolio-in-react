493399a3507c332a3a2c2406ca79bf96be587ed6
Your Name <you@example.com>
2020-12-01 15:28:22 +0000

    home page window popout

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 
 import styles from "./Home.module.scss"
 import image1 from "../../images/1.png"
-import image2 from "../../images/2.png"
-import image3 from "../../images/3.png"
-import flowerButton from "../../images/flower-button.png"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -16,27 +13,19 @@ class Home extends Component {
   state = {
       circleIsExpanded: false,
       image1Shows: false,
-      image2Shows: false,
-      image3Shows: false,
   };
   
   expandCircle = () => {
     this.setState({ circleIsExpanded: !this.state.circleIsExpanded });
     this.setState({ image1Shows: !this.state.image1Shows });
-    this.setState({ image2Shows: !this.state.image2Shows });
-    this.setState({ image3Shows: !this.state.image3Shows });
   };
 
 
   render() { 
 
-    const flipStyles = this.state.circleIsExpanded ? styles.buttonVanish : "";
+    const flipStyles = this.state.circleIsExpanded ? styles.buttonGrows : "";
 
     const showImage1 = this.state.image1Shows ? styles.slide1 : "";
-
-    const showImage2 = this.state.image1Shows ? styles.slide2 : "";
-
-    const showImage3 = this.state.image1Shows ? styles.slide3 : "";
 
 
     return ( 
@@ -44,43 +33,54 @@ class Home extends Component {
 
         <Fade delay={500} duration={2000} className={styles.fades}>  
           <h1 className={styles.homeTitle}>Welcome</h1>
-          {/* <button className={`${styles.growButton} ${flipStyles}`} onClick={() => this.expandCircle()}>
-            <img src={flowerButton} alt="flower button" className={styles.flowerButton}/>
-          </button> */}
         </Fade>
 
         <Slide triggerOnce delay={1000} duration={2000}>
-         <Fade delay={1000} duration={1200} className={styles.fades}> 
+          <Fade delay={1000} duration={1200} className={styles.fades}> 
             <div className={styles.arrowSlideAcross1}>
               <FontAwesomeIcon icon={['fas', 'angle-double-right']}/>
             </div>
           </Fade>
         </Slide>
 
-         <Fade delay={2600} duration={500} className={styles.fades}> 
-            <div className={styles.arrowSlideAcross2}>
-              <FontAwesomeIcon icon={['fas', 'angle-double-right']}/>
-            </div>
-          </Fade>
+        <Fade delay={2600} duration={500} className={styles.fades}> 
+          <div className={styles.arrowSlideAcross2}>
+            <FontAwesomeIcon icon={['fas', 'angle-double-right']}/>
+          </div>
+        </Fade>
         
-         <Fade delay={2500} duration={500} className={styles.fades}> 
-            <div className={styles.arrowSlideAcross3}>
-              <FontAwesomeIcon icon={['fas', 'angle-double-right']}/>
-            </div>
-          </Fade>
+        <Fade delay={2500} duration={500} className={styles.fades}> 
+          <div className={styles.arrowSlideAcross3}>
+            <FontAwesomeIcon icon={['fas', 'angle-double-right']}/>
+          </div>
+        </Fade>
         
-         <Fade delay={2400} duration={500} className={styles.fades}> 
-            <div className={styles.arrowSlideAcross4}>
-              <FontAwesomeIcon icon={['fas', 'angle-double-right']}/>
-            </div>
-          </Fade>
-        
+        <Fade delay={2400} duration={500} className={styles.fades}> 
+          <div className={styles.arrowSlideAcross4}>
+            <FontAwesomeIcon icon={['fas', 'angle-double-right']}/>
+          </div>
+        </Fade>
 
-         {/* <Slide triggerOnce delay={1000} duration={2000} className={`${showImage1} ${styles.plant1}`}>
+        <Fade delay={2700} duration={500} className={styles.fades}> 
+          <div className={styles.arrowSlideDown1}>
+            <FontAwesomeIcon icon={['fas', 'angle-double-right']}/>
+          </div>
+        </Fade>
+
+        <Fade delay={4500} duration={2000} className={styles.fades}>
+          <button className={`${styles.growButton} ${flipStyles}`} onClick={() => this.expandCircle()}>
+          </button>
+        </Fade>
+
+
+        
+        
+        <Fade triggerOnce delay={1800} duration={700} className={`${showImage1} ${styles.pictureOfLouis}`}>
           <img src={image1} alt="1" className={styles.image1}/>
-        </Slide>
+        </Fade>
         
         
+        {/*
         <Slide triggerOnce delay={1600} duration={2000} className={`${showImage2} ${styles.plant2}`}>
           <img src={image2} alt="2" className={styles.image2}/>
         </Slide>   
