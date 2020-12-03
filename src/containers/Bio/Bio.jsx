8e88cce6ee3d44c2387df70e7f18b3fd8eaccf5e
Your Name <you@example.com>
2020-12-03 15:11:09 +0000

    less sea sickness

--- src/containers/Bio/Bio.jsx
+++ src/containers/Bio/Bio.jsx
@@ -24,13 +24,16 @@ class Bio extends Component {
         <h1 className={styles.bioTitle}>
           Biography        
         </h1>
-        <p className={styles.slow}>My introduction to coding began during university whilst studying plant biology and learning python. I worked within a team who were focussed on analysing gene mutations affecting root growth patterns for the purpose of improving crop performance. They had created a program called RootNav to track and quantify these growth patterns, and I saw how many thousands of hours of labour could be saved through some intuitive programming.</p>
 
-        <p className={styles.slow2}>I began to recognise more potential applications for software, and with this my interest only developed. I decided to refocus my learning predominantly in the direction of coding, and this commenced with an intensive coding boot camp, where I was introduced to an array of languages and libraries. Since this experience, my interest and desire to learn and contribute have increased exponentially, as every new skill has widened my perception of what is possible.</p>
+        <div className={styles.bioParagraphsContainer}>
+        <p className={styles.bioParagraph}>My introduction to coding began during university whilst studying plant biology and learning python. I worked within a team who were focussed on analysing gene mutations affecting root growth patterns for the purpose of improving crop performance. They had created a program called RootNav to track and quantify these growth patterns, and I saw how many thousands of hours of labour could be saved through some intuitive programming.</p>
 
-        <p className={styles.slow3}>What fascinates me at present is the connection between form and function, and the subtle UI and UX design considerations orchestrated throughout websites and applications to make them both appealing and responsive to the user.</p>
+        <p className={styles.bioParagraph}>I began to recognise more potential applications for software, and with this my interest only developed. I decided to refocus my learning predominantly in the direction of coding, and this commenced with an intensive coding boot camp, where I was introduced to an array of languages and libraries. Since this experience, my interest and desire to learn and contribute have increased exponentially, as every new skill has widened my perception of what is possible.</p>
 
-        <div className={`${styles.bioTitle} ${styles.popOut} ${changePopout}`} onClick={() => this.openPopout()}>
+        <p className={styles.bioParagraph}>What fascinates me at present is the connection between form and function, and the subtle UI and UX design considerations orchestrated throughout websites and applications to make them both appealing and responsive to the user.</p>
+        </div>
+
+        <h2 className={`${styles.bioTitle} ${styles.popOut} ${changePopout}`} onClick={() => this.openPopout()}>
           Hobbies and Interests <FontAwesomeIcon icon={['fas', 'angle-double-right']} className={styles.arrowIcon}/>
           <div>
             <p className={styles.interestParagraph}>Green Spaces and Urban Farming</p>
@@ -39,7 +42,7 @@ class Bio extends Component {
             <p className={styles.interestParagraph}>Mycology</p>
             <p className={styles.interestParagraph}>Art and Design</p>
           </div>
-        </div>
+        </h2>
 
         <div className={styles.transparentBox}></div>
         
